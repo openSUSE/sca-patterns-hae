7bf89eef966e32aab5e329a54ed2d02febb3d398
Jason Record <g23guy@gmail.com>
2014-01-29 08:36:17 -0700

    includes pertinent file from sca-patterns-basic

---
+++ patterns/HAE/bhc-heartbeat-00001.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+# Title:       Heartbeat Basic Service Pattern
+# Description: Checks to see if Heartbeat clustering is installed, valid and running
+# Modified:    2013 Jun 21
+
+##############################################################################
+#  Copyright (C) 2013 SUSE LLC
+##############################################################################
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; version 2 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+
+##############################################################################
+
+##############################################################################
+# Module Definition
+##############################################################################
+
+
+use strict;
+use warnings;
+use SDP::Core;
+use SDP::SUSE;
+
+##############################################################################
+# Overriden (eventually or in part) from SDP::Core Module
+##############################################################################
+
+@PATTERN_RESULTS = (
+	PROPERTY_NAME_CLASS."=Basic Health",
+	PROPERTY_NAME_CATEGORY."=HAE",
+	PROPERTY_NAME_COMPONENT."=Heartbeat",
+	PROPERTY_NAME_PATTERN_ID."=$PATTERN_ID",
+	PROPERTY_NAME_PRIMARY_LINK."=META_LINK_TID",
+	PROPERTY_NAME_OVERALL."=$GSTATUS",
+	PROPERTY_NAME_OVERALL_INFO."=None",
+	"META_LINK_TID=http://www.suse.com/support/kb/doc.php?id=7001417"
+);
+
+
+
+
+##############################################################################
+# Program execution functions
+##############################################################################
+
+	my $CHECK_PACKAGE = "heartbeat";
+	my $CHECK_SERVICE = "heartbeat";
+	my $FILE_SERVICE = "ha.txt";
+
+SDP::Core::processOptions();
+	if ( SDP::SUSE::compareKernel(SLE11GA) < 0 ) {
+		if ( packageInstalled($CHECK_PACKAGE) ) {
+			SDP::SUSE::serviceHealth($FILE_SERVICE, $CHECK_PACKAGE, $CHECK_SERVICE);
+		} else {
+			SDP::Core::updateStatus(STATUS_ERROR, "Basic Service Health; Package Not Installed: $CHECK_PACKAGE");
+		}
+	} else {
+		SDP::Core::updateStatus(STATUS_ERROR, "Outside Kernel Scope, Requires SLE10 or lower");
+	}
+SDP::Core::printPatternResults();
+
+exit;
+
--- spec/sca-patterns-hae.spec
+++ spec/sca-patterns-hae.spec
@@ -12,7 +12,7 @@
 %define patuser root
 %define patgrp root
 %define mode 544
-%define category HAE
+%define category SLE
 
 Name:         sca-patterns-hae
 Summary:      Supportconfig Analysis Patterns for HAE
@@ -21,7 +21,7 @@ Group:        Documentation/SuSE
 License:      GPL-2.0
 Autoreqprov:  on
 Version:      1.3
-Release:      1
+Release:      2
 Source:       %{name}-%{version}.tar.gz
 BuildRoot:    %{_tmppath}/%{name}-%{version}
 Buildarch:    noarch
@@ -44,20 +44,29 @@ Authors:
 %install
 pwd;ls -la
 rm -rf $RPM_BUILD_ROOT
-install -d $RPM_BUILD_ROOT/%{patdir}/%{category}
-install -m %{mode} patterns/%{category}/* $RPM_BUILD_ROOT/%{patdir}/%{category}
+install -d $RPM_BUILD_ROOT/%{patdir}/%{category}/sle10all
+install -d $RPM_BUILD_ROOT/%{patdir}/%{category}/sle11all
+install -m %{mode} patterns/HAE/* $RPM_BUILD_ROOT/%{patdir}/%{category}/sle10all
+install -m %{mode} patterns/HAE/* $RPM_BUILD_ROOT/%{patdir}/%{category}/sle11all
 
 %files
 %defattr(-,%{patuser},%{patgrp})
 %dir %{patdirbase}
 %dir %{patdir}
 %dir %{patdir}/%{category}
-%attr(%{mode},%{patuser},%{patgrp}) %{patdir}/%{category}/*
+%dir %{patdir}/%{category}/sle10all
+%dir %{patdir}/%{category}/sle11all
+%attr(%{mode},%{patuser},%{patgrp}) %{patdir}/%{category}/sle10all/*
+%attr(%{mode},%{patuser},%{patgrp}) %{patdir}/%{category}/sle11all/*
 
 %clean
 rm -rf $RPM_BUILD_ROOT
 
 %changelog
+* Tue Jan 28 2014 jrecord@suse.com
+- includes pertinent patterns from sca-patterns-basic
+- moved patterns from own directory to SLE structure
+
 * Thu Jan 16 2014 jrecord@suse.com
 - relocated files according to FHS
 
